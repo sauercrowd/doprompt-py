ae75bab63dbe6a29ab0383a4348cd2d76da8a21b
Jonas Otten <jonadev95@posteo.org>
2025-01-21 19:03:53 +0000

    fix tests

--- tests/dotprompt_test.py
+++ tests/dotprompt_test.py
@@ -1,50 +1,17 @@
 from doprompt.doprompt import DoPrompt
-from openai import OpenAI
-import os
-
-#def test_parser():
-#    prompt = DoPrompt("./tests/simple.prompt")
-#    result = prompt.render_prompt(location="New York").strip()
-#    assert result == "You are the world's most welcoming AI assistant and are currently working in New York."
-#    result = prompt.render_prompt(location="New York", is_true=True).strip()
-#    assert result == "You are the world's most welcoming AI assistant and are currently working in New York.\nThis is a true statement."
-
 
 def test_execute_prompt():
     prompt = DoPrompt("./tests/chat.prompt")
 
-    openai = OpenAI()
+    rendered_messages = prompt.get_messages(values={
+        'userQuestion': 'What is the capital of France?',
+    })
+                                                
 
-    chat_completion = openai.chat.completions.create(
-        messages=prompt.get_messages(values={'userQuestion': 'What is the capital of France?',
-                                             'authors': [{'hello': 1}]}),
-        model=prompt.model_name
-    )
-
-    print(chat_completion.choices[0].message.content)
-
-    raise ValueError('Test failed')
+    assert rendered_messages == [
+        {'role': 'system', 'content': 'You are a helpful AI assistant that really loves to talk about food. Try to work\nfood items into all of your conversations.'},
+        {'role': 'user', 'content': 'What is the capital of France?'}
+    ]
 
 
-
-#def test_jinja():
-#    prompt = DotPrompt("./tests/simple_jinja.prompt")
-#    result = prompt.render_prompt(location="London").strip()
-#    assert result == "You are the world's most welcoming AI assistant and are currently working in London."
-#    result = prompt.render_prompt(location="London", is_true=True).strip()
-#    assert result == "You are the world's most welcoming AI assistant and are currently working in London.\n\nThis is a true statement."
-#
-#def test_proxy():
-#    prompt = DotPrompt("./tests/simple_proxy.prompt")
-#
-#    from openai import OpenAI
-#
-#    client = OpenAI(
-#        api_key=os.environ.get("PROMPTKINS_API_KEY"),
-#        base_url="http://localhost:8080",
-#    )
-#
-#    chat_completion = client.chat.completions.create(
-#        messages=[{"role": "user", "content": "Say this is a test!"}],
-#        model="gpt-4o-mini",
-#    )
+    assert prompt.model_name == 'gpt-4o'
